415aed2be63902cbf953650d7a17e03d87197d89
MatchaOnMuffins <henryzhangjcle@gmail.com>
2022-06-22 00:35:14 -0400

    Added comments

--- MaestroControl.hpp
+++ MaestroControl.hpp
@@ -9,7 +9,9 @@
 #define O_NOCTTY 0
 #include <Windows.h>
 #else
+
 #include <termios.h>
+
 #endif
 
 class MaestroControl {
@@ -17,21 +19,43 @@ private:
     int fd;
     const char *device;
 public:
-    void reconnect() {
-        fd = open(device, O_RDWR | O_NOCTTY);
-        perror("Reconnecting");
-    }
+
+    /**
+     * @brief MaestroControl constructor
+     * @param dev device name (e.g. /dev/ttyACM0)
+     */
 
     explicit MaestroControl(const char *dev) {
         device = dev;
         fd = open(device, O_RDWR | O_NOCTTY);
     }
 
+    /**
+     * @brief Reconnects if connection is lost
+     */
+
+    void reconnect() {
+        fd = open(device, O_RDWR | O_NOCTTY);
+        perror("Reconnecting");
+    }
+
+    /**
+     * @brief Returns the serial port name
+     * @return Serial port name (e.g. /dev/ttyACM0)
+     */
+
     std::string getSerialPort() {
         std::ostringstream buffer;
         buffer << device;
         return buffer.str();
     }
+
+    /**
+    * @brief Sets position of servo to given value
+    * @param channel channel number
+    * @param position servo position (4000-8000)
+    * @return -1 if an error occurred else 0 if successful
+    */
 
     int setPosition(unsigned char channel, unsigned short target) {
         unsigned char command[] = {0x84, channel, static_cast<unsigned char>(target & 0x7F),
@@ -43,6 +67,12 @@ public:
         }
         return 0;
     }
+
+    /**
+     * @brief Gets position of servo
+     * @param channel channel number
+     * @return position of servo, or -1 if error
+     */
 
     int getPosition(unsigned char channel) {
         unsigned char command[] = {0x90, channel};
@@ -62,6 +92,9 @@ public:
         return response[0] + 256 * response[1];
     }
 
+    /**
+     * @brief Destructor
+     */
     void destroy() const {
         close(fd);
     }
